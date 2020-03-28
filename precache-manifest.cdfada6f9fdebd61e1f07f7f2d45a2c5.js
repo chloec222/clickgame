082e56143192110f9b8d3080c4cc3ab947e0e9e3
CC <55222885+chloec222@users.noreply.github.com>
2020-03-27 22:29:55 -0400

    Updates

---
+++ precache-manifest.cdfada6f9fdebd61e1f07f7f2d45a2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681fae12d5110eabe0edb30288bb2fd3",
+    "url": "/clickgame/index.html"
+  },
+  {
+    "revision": "2a078aa31dd7c6d86b3b",
+    "url": "/clickgame/static/css/main.252eb3e6.chunk.css"
+  },
+  {
+    "revision": "c9171809ddd1156558a8",
+    "url": "/clickgame/static/js/2.2c7a26e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickgame/static/js/2.2c7a26e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a078aa31dd7c6d86b3b",
+    "url": "/clickgame/static/js/main.d21e07f9.chunk.js"
+  },
+  {
+    "revision": "582b443d17ae06930a82",
+    "url": "/clickgame/static/js/runtime-main.05b098c2.js"
+  }
+]);
